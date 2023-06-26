caa697929d429c0b47c718fafc25cafc541a8158
AriadneMTS <107881306+AriadneMTS@users.noreply.github.com>
2023-06-26 13:36:07 -0300

    erro

---
+++ PageCurso/index.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import axios from "axios";
+import { useRouter } from "next/navigation"
+import { useCallback, useState } from "react";
+
+interface interProps {
+    dados: Array<{ id: number, nome: string, coordenador: string, duracao: number }>
+}
+
+export default function PageCurso(props: interProps) {
+
+    const router = useRouter();
+
+    const [dados, setDados] = useState<Array<{
+        id: number,
+        nome: string,
+        coordenador: string, 
+        duracao: number
+    }
+    >>(props.dados)
+
+    const excluirCategoria = useCallback((id: number) => {
+        axios.delete('http://localhost:8000/api/cursos/'+id)
+        .then((res) => {
+            
+        })
+    }, [])
+
+    return (
+        <>
+            <div
+                className="d-flex justify-content-between"
+            >
+                <h1>Curso</h1>
+                <div>
+                    <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={() => {
+                            router.push('/curso/criar')
+                        }}
+                    >
+                        Adicionar
+                    </button>
+                </div>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        dados.map((element) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        {element.id}
+                                    </td>
+                                    <td>
+                                        {element.nome}
+                                    </td>
+                                    <td>
+                                        <button
+                                            className="btn btn-primary"
+                                            type="button"
+                                            onClick={() => {
+                                                router.push('/curso/' + element.id)
+                                            }}
+                                        >
+                                            Editar
+                                        </button>
+                                        <button
+                                            className="btn btn-danger"
+                                            type="button"
+                                            onClick={() => {
+                                                excluirCategoria(element.id)
+                                            }}
+                                        >
+                                            Excluir
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
+        </>
+    )
+}
---
+++ autor/page.tsx
@@ -0,0 +1,46 @@
+import { cookies } from 'next/headers'
+import { LayoutDashboard } from "@/components/LayoutDashboard";
+import { verificaTokenExpirou } from '@/services/Token';
+import { redirect } from 'next/navigation';
+import axios from 'axios';
+import PageEditora from '@/components/PageAutor';
+import PageAutor from '@/components/PageAutor';
+
+interface interAutor {
+    "id": number;
+    "nome": string;
+    "endereco": string;
+    "cidade": string;
+    "uf": string;
+    "telefone": number;
+}
+
+export default async function Autor() {
+
+    const cookie = cookies();
+
+    const token = cookie.get('biblioteca-react.token')
+
+    if (!token?.value || verificaTokenExpirou(token.value)) {
+        redirect('/login')
+    }
+
+    let autor: { data: Array<interAutor> } = { data: [] }
+
+    try {
+      autor = await axios.get('http://127.0.0.1:8000/api/autor');
+        console.log({autor})
+    } catch (error) {
+        console.log({error})
+    }
+
+
+    return (
+        <LayoutDashboard
+            active='autor'
+            token={token.value}
+        >
+            <PageAutor dados={autor.data} />
+        </LayoutDashboard>
+    )
+}
