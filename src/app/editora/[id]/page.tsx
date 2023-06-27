2d43f282efae8deaa90d47eaffde7f80cffb11d7
Gustavo Henrique <110069339+Guhhzz@users.noreply.github.com>
2023-06-27 00:51:49 +0000

    PageLivro

--- src/app/editora/[id]/page.tsx
+++ src/app/editora/[id]/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { cookies } from 'next/headers'
 import { LayoutDashboard } from "@/components/LayoutDashboard";
 import { verificaTokenExpirou } from '@/services/Token';
@@ -24,3 +25,25 @@ export default async function AddEditora({params}: any) {
         </LayoutDashboard>
     )
 }
+=======
+import { cookies } from 'next/headers';
+import { LayoutDashboard } from '@/components/LayoutDashboard';
+import { verificaTokenExpirou } from '@/services/Token';
+import { redirect } from 'next/navigation';
+import PageFormEditora from '@/components/PageFormCategoria';
+
+export default async function AddEditora({ params }: any) {
+  const cookie = cookies();
+  const token = cookie.get('shoopypainel.token');
+
+  if (!token?.value || verificaTokenExpirou(token.value)) {
+    redirect('/login');
+  }
+
+  return (
+    <LayoutDashboard active="editora" token={token.value}>
+      <PageFormEditora parametro={params.id} />
+    </LayoutDashboard>
+  );
+}
+>>>>>>> a11e47f (PageLivro)
